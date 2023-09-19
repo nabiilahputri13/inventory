f1fd21c7455f97ebdd7256dff50861ad824a28d3
nabiilahputri13 <putrisafa1394@gmail.com>
2023-09-19 14:15:29 +0700

    add bonus tugas3

--- main/views.py
+++ main/views.py
@@ -8,10 +8,12 @@ from django.core import serializers
 # Create your views here.
 def show_main(request):
     products = Product.objects.all()
+    count = Product.objects.all().count()
     context = {
         'name': 'Nabiilah Putri Safa',
         'class': 'PBP A',
-        'products': products
+        'products': products,
+        'count': count
     }
 
     return render(request, "main.html", context)
@@ -29,7 +31,6 @@ def create_product(request):
 def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-
 
 def show_json(request):
     data = Product.objects.all()
